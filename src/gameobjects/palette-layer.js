9002a3eb2ddf1e97be4ebc65df733a4a70b57656
Jonah Jeleniewski <cirrasbu@hotmail.com>
2021-10-01 20:52:13 +1000

    Rewrite palette renderer

---
+++ src/gameobjects/palette-layer.js
@@ -0,0 +1,431 @@
+export class PaletteLayerEntry {
+  constructor(textureCache, width, height) {
+    this.textureCache = textureCache;
+    this.width = width;
+    this.height = height;
+    this._x = 0;
+    this._y = 0;
+    this._visible = false;
+    this._cacheEntry = null;
+  }
+
+  _getCacheEntry() {
+    throw new Error("PaletteLayerEntry._getCacheEntry() must be implemented");
+  }
+
+  preload() {
+    this._getCacheEntry();
+  }
+
+  get id() {
+    throw new Error("PaletteLayerEntry.id() must be implemented");
+  }
+
+  get visible() {
+    return this._visible;
+  }
+
+  set visible(visible) {
+    if (this.visible !== visible) {
+      if (visible) {
+        this.cacheEntry.incRef();
+      } else {
+        this.cacheEntry.decRef();
+      }
+      this._visible = visible;
+    }
+  }
+
+  get cacheEntry() {
+    if (this._cacheEntry === null) {
+      this._cacheEntry = this._getCacheEntry();
+    }
+    return this._cacheEntry;
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  set x(value) {
+    this._x = value;
+    if (this.sprite) {
+      this.sprite.x = value;
+    }
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  set y(value) {
+    this._y = value;
+    if (this.sprite) {
+      this.sprite.y = value;
+    }
+  }
+}
+
+export class PaletteLayerResourceEntry extends PaletteLayerEntry {
+  constructor(textureCache, width, height, fileID, resourceID) {
+    super(textureCache, width, height);
+    this.fileID = fileID;
+    this.resourceID = resourceID;
+  }
+
+  _getCacheEntry() {
+    return this.textureCache.getResource(this.fileID, this.resourceID);
+  }
+
+  get id() {
+    return this.resourceID - 100;
+  }
+}
+
+export class PaletteLayerBlackTileEntry extends PaletteLayerEntry {
+  constructor(textureCache) {
+    super(textureCache, 64, 32);
+  }
+
+  _getCacheEntry() {
+    return this.textureCache.getBlackTile();
+  }
+
+  get id() {
+    return 0;
+  }
+}
+
+export class PaletteLayerSpecEntry extends PaletteLayerEntry {
+  constructor(textureCache, tileSpec) {
+    super(textureCache, 64, 32);
+    this.tileSpec = tileSpec;
+  }
+
+  _getCacheEntry() {
+    return this.textureCache.getSpec(this.tileSpec);
+  }
+
+  get id() {
+    return this.tileSpec;
+  }
+}
+
+export class PaletteLayer extends Phaser.GameObjects.GameObject {
+  static SECTION_HEIGHT = 512;
+
+  constructor(scene) {
+    super(scene, "PaletteLayer");
+    this.scene = scene;
+    this.entries = new Map();
+    this.sections = [];
+    this.renderList = new Set();
+    this._selectedEntry = null;
+    this._width = 0;
+    this._height = 0;
+    this.scroll = 0;
+    this.dirty = true;
+    this.animationFrame = 0;
+
+    this.initPipeline();
+  }
+
+  addEntry(entry) {
+    this.entries.set(entry.id, entry);
+  }
+
+  getEntryAtPosition(x, y) {
+    for (let entry of this.renderList) {
+      let top = entry.y;
+      let bottom = top + entry.height;
+      let left = entry.x;
+      let right = left + entry.width;
+      if (top < y && bottom > y && left < x && right > x) {
+        return entry;
+      }
+    }
+    return null;
+  }
+
+  selectEntry(drawID) {
+    this._selectedEntry = this.entries.get(drawID) || null;
+  }
+
+  set visible(visible) {
+    if (this.visible === visible) {
+      return;
+    }
+
+    if (visible) {
+      this.layout();
+    } else {
+      for (let entry of this.entries.values()) {
+        entry.visible = false;
+      }
+    }
+
+    super.visible = visible;
+  }
+
+  cull() {
+    let visibleSections = [];
+    let cullTop = this.scroll;
+    let cullBottom = cullTop + this.scene.cameras.main.height;
+
+    for (let i = 0; i < this.sections.length; ++i) {
+      let section = this.sections[i];
+      let sectionTop = i * PaletteLayer.SECTION_HEIGHT;
+      let sectionBottom = sectionTop + PaletteLayer.SECTION_HEIGHT;
+      let show = sectionBottom > cullTop && sectionTop < cullBottom;
+      if (show) {
+        visibleSections.push(section);
+      } else {
+        for (let entry of section) {
+          entry.visible = false;
+        }
+      }
+    }
+
+    this.renderList.clear();
+    for (let section of visibleSections) {
+      for (let entry of section) {
+        entry.visible = true;
+        this.renderList.add(entry);
+      }
+    }
+  }
+
+  layout() {
+    if (this.dirty) {
+      this.layoutEntries();
+    }
+    this.cull();
+  }
+
+  calcSection(y) {
+    let section = y;
+    if (section !== 0) {
+      section = Math.trunc(section / PaletteLayer.SECTION_HEIGHT);
+    }
+    return section;
+  }
+
+  layoutEntries() {
+    const xRes = 32;
+    const yRes = 32;
+
+    let pageWidth = Math.floor(this.width / xRes);
+    let colHeights = new Array(pageWidth).fill(0);
+
+    this.sections.length = 0;
+    this._height = 0;
+
+    for (let entry of this.entries.values()) {
+      let width = entry.width;
+      let height = entry.height;
+
+      let blockWidth = Math.floor((width + xRes - 1) / xRes);
+      let blockHeight = Math.floor((height + yRes - 1) / yRes);
+
+      blockWidth = Math.min(blockWidth, pageWidth);
+
+      let startY = 0x7fffffff;
+
+      for (let colHeight of colHeights) {
+        startY = Math.min(startY, colHeight);
+      }
+
+      let foundPosition = false;
+
+      for (let y = startY; ; ++y) {
+        for (let x = 0; x <= pageWidth - blockWidth; ++x) {
+          let maxY = 0;
+
+          for (let i = x; i < x + blockWidth; ++i) {
+            maxY = Math.max(maxY, colHeights[i]);
+          }
+
+          if (maxY < y) {
+            for (let i = x; i < x + blockWidth; ++i) {
+              colHeights[i] = maxY + blockHeight;
+            }
+
+            entry.x = x * xRes;
+            entry.y = (y - 1) * yRes;
+
+            this._height = Math.max(entry.y + entry.height, this.height);
+
+            let startSection = this.calcSection(entry.y);
+            let endSection = this.calcSection(entry.y + entry.height);
+
+            let sectionsToAdd = endSection + 1 - this.sections.length;
+            for (let i = 0; i < sectionsToAdd; ++i) {
+              this.sections.push([]);
+            }
+
+            for (let i = startSection; i <= endSection; ++i) {
+              this.sections[i].push(entry);
+            }
+
+            foundPosition = true;
+            break;
+          }
+        }
+
+        if (foundPosition) {
+          break;
+        }
+      }
+    }
+
+    this.dirty = false;
+  }
+
+  renderWebGL(renderer, src, camera) {
+    let pipeline = renderer.pipelines.set(src.pipeline, src);
+    let getTint = Phaser.Renderer.WebGL.Utils.getTintAppendFloatAlpha;
+
+    renderer.pipelines.preBatch(src);
+
+    for (let entry of this.renderList) {
+      let frame = entry.cacheEntry.asset.getFrame(this.animationFrame);
+      let texture = frame.glTexture;
+      let textureUnit = pipeline.setTexture2D(texture, src);
+
+      let alpha = 1.0;
+      if (entry === this.selectedEntry) {
+        alpha = 0.6;
+      }
+
+      let tint = getTint(0xffffff, alpha);
+
+      pipeline.batchTexture(
+        src,
+        texture,
+        texture.width,
+        texture.height,
+        entry.x,
+        entry.y,
+        entry.width,
+        entry.height,
+        1,
+        1,
+        0,
+        false,
+        true,
+        1,
+        1,
+        0,
+        0,
+        frame.cutX,
+        frame.cutY,
+        frame.width,
+        frame.height,
+        tint,
+        tint,
+        tint,
+        tint,
+        false,
+        0,
+        0,
+        camera,
+        null,
+        true,
+        textureUnit
+      );
+    }
+
+    renderer.pipelines.postBatch(src);
+  }
+
+  renderCanvas(renderer, _src, camera) {
+    let ctx = renderer.currentContext;
+    ctx.save();
+
+    ctx.transform(1, 0, 0, 1, -camera.scrollX, -camera.scrollY);
+
+    if (!renderer.antialias) {
+      ctx.imageSmoothingEnabled = false;
+    }
+
+    for (let entry of this.renderList) {
+      ctx.save();
+
+      let alpha = 1.0;
+      if (entry === this.selectedEntry) {
+        alpha = 0.6;
+      }
+
+      ctx.globalAlpha = alpha;
+
+      let frame = entry.cacheEntry.asset.getFrame(this.animationFrame);
+      let cd = frame.canvasData;
+      let frameX = cd.x;
+      let frameY = cd.y;
+      let frameWidth = frame.cutWidth;
+      let frameHeight = frame.cutHeight;
+      let res = frame.source.resolution;
+
+      ctx.drawImage(
+        frame.source.image,
+        frameX,
+        frameY,
+        frameWidth,
+        frameHeight,
+        entry.x,
+        entry.y,
+        frameWidth / res,
+        frameHeight / res
+      );
+
+      ctx.restore();
+    }
+
+    ctx.restore();
+  }
+
+  update(_time, _delta) {
+    if (this.scene.cameras.main.dirty) {
+      this.cull();
+    }
+
+    this.animationFrame = Math.trunc(performance.now() / 600) % 4;
+  }
+
+  destroy(fromScene) {
+    for (let entry in this.entries.values()) {
+      entry.visible = false;
+    }
+    super.destroy(fromScene);
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  set width(value) {
+    this._width = value;
+    this.dirty = true;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  get selectedEntry() {
+    return this._selectedEntry;
+  }
+}
+
+Phaser.Class.mixin(PaletteLayer, [
+  Phaser.GameObjects.Components.BlendMode,
+  Phaser.GameObjects.Components.Depth,
+  Phaser.GameObjects.Components.Pipeline,
+  Phaser.GameObjects.Components.Visible,
+]);
+
+Phaser.GameObjects.GameObjectFactory.register("paletteLayer", function (scene) {
+  const layer = new PaletteLayer(scene);
+  this.displayList.add(layer);
+  return layer;
+});
--- src/scenes/palette-scene.js
+++ src/scenes/palette-scene.js
@@ -1,336 +1,10 @@
+import {
+  PaletteLayerResourceEntry,
+  PaletteLayerSpecEntry,
+  PaletteLayerBlackTileEntry,
+} from "../gameobjects/palette-layer";
 import { TextureCache } from "../gfx/texture-cache";
 
-class PaletteLayerEntry {
-  constructor(layer, width, height) {
-    this.layer = layer;
-    this.width = width;
-    this.height = height;
-    this._x = 0;
-    this._y = 0;
-    this.sprite = null;
-    this.pointerDown = false;
-  }
-
-  show() {
-    if (this.sprite) {
-      return;
-    }
-
-    let cacheEntry = this.getCacheEntry();
-    cacheEntry.incRef();
-
-    if (cacheEntry.asset.animation) {
-      this.sprite = this.layer.scene.add
-        .sprite(this.x, this.y)
-        .play(cacheEntry.asset.animation);
-      this.layer.scene.syncToMasterAnimation(this.sprite);
-    } else {
-      this.sprite = this.layer.scene.add.sprite(
-        this.x,
-        this.y,
-        cacheEntry.asset.textureKey,
-        cacheEntry.asset.frameKey
-      );
-    }
-
-    if (this.selected) {
-      this.sprite.setAlpha(0.6);
-    }
-
-    this.sprite.setOrigin(0);
-    this.sprite.setInteractive();
-
-    this.sprite.on("pointerup", (pointer) => {
-      if (pointer.leftButtonReleased() && pointer.getDistance() < 16) {
-        this.select();
-        this.pointerDown = false;
-      }
-    });
-  }
-
-  hide() {
-    if (!this.sprite) {
-      return;
-    }
-
-    this.getCacheEntry().decRef();
-    this.sprite.destroy();
-    this.sprite = null;
-  }
-
-  select() {
-    if (this.layer.selectedResource) {
-      this.layer.selectedResource.unselect();
-    }
-
-    this.layer.selectedResource = this;
-    if (this.sprite) {
-      this.sprite.setAlpha(0.6);
-    }
-  }
-
-  unselect() {
-    this.layer.selectedResource = null;
-    if (this.sprite) {
-      this.sprite.setAlpha(1);
-    }
-  }
-
-  getCacheEntry() {
-    throw new Error("PaletteLayerEntry.getCacheEntry() must be implemented");
-  }
-
-  preload() {
-    this.getCacheEntry();
-  }
-
-  get id() {
-    throw new Error("PaletteLayerEntry.id() must be implemented");
-  }
-
-  get selected() {
-    return this.layer.selectedResource === this;
-  }
-
-  get x() {
-    return this._x;
-  }
-
-  set x(value) {
-    this._x = value;
-    if (this.sprite) {
-      this.sprite.x = value;
-    }
-  }
-
-  get y() {
-    return this._y;
-  }
-
-  set y(value) {
-    this._y = value;
-    if (this.sprite) {
-      this.sprite.y = value;
-    }
-  }
-}
-
-class PaletteLayerResourceEntry extends PaletteLayerEntry {
-  constructor(layer, width, height, fileID, resourceID) {
-    super(layer, width, height);
-    this.fileID = fileID;
-    this.resourceID = resourceID;
-  }
-
-  getCacheEntry() {
-    return this.layer.scene.textureCache.getResource(
-      this.fileID,
-      this.resourceID
-    );
-  }
-
-  get id() {
-    return this.resourceID - 100;
-  }
-}
-
-class PaletteLayerBlackTileEntry extends PaletteLayerEntry {
-  constructor(layer) {
-    super(layer, 64, 32);
-  }
-
-  getCacheEntry() {
-    return this.layer.scene.textureCache.getBlackTile();
-  }
-
-  get id() {
-    return 0;
-  }
-}
-
-class PaletteLayerSpecEntry extends PaletteLayerEntry {
-  constructor(layer, tileSpec) {
-    super(layer, 64, 32);
-    this.tileSpec = tileSpec;
-  }
-
-  getCacheEntry() {
-    return this.layer.scene.textureCache.getSpec(this.tileSpec);
-  }
-
-  get id() {
-    return this.tileSpec;
-  }
-}
-
-class PaletteLayer {
-  static SECTION_HEIGHT = 512;
-
-  constructor(scene) {
-    this.scene = scene;
-    this.entries = new Map();
-    this.sections = [];
-    this._selectedResource = null;
-    this.width = 0;
-    this.height = 0;
-    this.scroll = 0;
-    this.dirty = true;
-  }
-
-  hide() {
-    for (let resource of this.entries.values()) {
-      resource.hide();
-    }
-  }
-
-  show() {
-    this.prioritizePreloads();
-    this.layout();
-    this.scene.updateScroll();
-    this.scene.updateSelectedDrawID();
-  }
-
-  prioritizePreloads() {
-    let preloads = this.scene.preloads;
-    for (let preload of preloads) {
-      if (preload.layer === this) {
-        Phaser.Utils.Array.MoveTo(preloads, preload, 0);
-        break;
-      }
-    }
-  }
-
-  cull() {
-    let visibleSections = [];
-    let cullTop = this.scroll;
-    let cullBottom = cullTop + this.scene.cameras.main.height;
-
-    for (let i = 0; i < this.sections.length; ++i) {
-      let section = this.sections[i];
-      let sectionTop = i * PaletteLayer.SECTION_HEIGHT;
-      let sectionBottom = sectionTop + PaletteLayer.SECTION_HEIGHT;
-      let show = sectionBottom > cullTop && sectionTop < cullBottom;
-      if (show) {
-        visibleSections.push(section);
-      } else {
-        for (let resource of section) {
-          resource.hide();
-        }
-      }
-    }
-
-    for (let section of visibleSections) {
-      for (let resource of section) {
-        resource.show();
-      }
-    }
-  }
-
-  layout() {
-    if (this.dirty) {
-      this.layoutEntries();
-    }
-    this.cull();
-    this.scene.data.set("contentHeight", this.height);
-  }
-
-  calcSection(y) {
-    let section = y;
-    if (section !== 0) {
-      section = Math.trunc(section / PaletteLayer.SECTION_HEIGHT);
-    }
-    return section;
-  }
-
-  layoutEntries() {
-    const xRes = 32;
-    const yRes = 32;
-
-    let pageWidth = Math.floor(this.width / xRes);
-    let colHeights = new Array(pageWidth).fill(0);
-
-    this.sections.length = 0;
-    this.height = 0;
-
-    for (let entry of this.entries.values()) {
-      let width = entry.width;
-      let height = entry.height;
-
-      let blockWidth = Math.floor((width + xRes - 1) / xRes);
-      let blockHeight = Math.floor((height + yRes - 1) / yRes);
-
-      blockWidth = Math.min(blockWidth, pageWidth);
-
-      let startY = 0x7fffffff;
-
-      for (let colHeight of colHeights) {
-        startY = Math.min(startY, colHeight);
-      }
-
-      let foundPosition = false;
-
-      for (let y = startY; ; ++y) {
-        for (let x = 0; x <= pageWidth - blockWidth; ++x) {
-          let maxY = 0;
-
-          for (let i = x; i < x + blockWidth; ++i) {
-            maxY = Math.max(maxY, colHeights[i]);
-          }
-
-          if (maxY < y) {
-            for (let i = x; i < x + blockWidth; ++i) {
-              colHeights[i] = maxY + blockHeight;
-            }
-
-            entry.x = x * xRes;
-            entry.y = (y - 1) * yRes;
-
-            this.height = Math.max(entry.y + entry.height, this.height);
-
-            let startSection = this.calcSection(entry.y);
-            let endSection = this.calcSection(entry.y + entry.height);
-
-            let sectionsToAdd = endSection + 1 - this.sections.length;
-            for (let i = 0; i < sectionsToAdd; ++i) {
-              this.sections.push([]);
-            }
-
-            for (let i = startSection; i <= endSection; ++i) {
-              this.sections[i].push(entry);
-            }
-
-            foundPosition = true;
-            break;
-          }
-        }
-
-        if (foundPosition) {
-          break;
-        }
-      }
-    }
-
-    this.dirty = false;
-  }
-
-  get width() {
-    return this._width;
-  }
-
-  set width(value) {
-    this._width = value;
-    this.dirty = true;
-  }
-
-  get selectedResource() {
-    return this._selectedResource;
-  }
-
-  set selectedResource(value) {
-    this._selectedResource = value;
-    this.scene.updateSelectedDrawID();
-  }
-}
 class LayerPreload {
   static PRELOAD_PER_FRAME = 5;
 
@@ -364,9 +38,9 @@ export class PaletteScene extends Phaser.Scene {
     super("palette");
     this.firstUpdate = true;
     this.textureCache = null;
+    this.selectedLayer = null;
     this.layers = [];
     this.preloads = [];
-    this.masterAnimation = null;
     this.lastResize = performance.now();
   }
 
@@ -380,37 +54,24 @@ export class PaletteScene extends Phaser.Scene {
 
     this.layers = this.createLayers();
     this.preloads = this.layers.map((layer) => new LayerPreload(layer));
-    this.masterAnimation = this.createMasterAnimation();
 
     this.data.events.on(
       "changedata-selectedLayer",
-      (_parent, value, previousValue) => {
-        this.layers[previousValue].hide();
-        this.layers[value].show();
+      (_parent, value, _previousValue) => {
+        this.selectLayer(value);
       }
     );
 
     this.data.events.on(
       "changedata-eyedrop",
       (_parent, value, _previousValue) => {
-        if (value.drawID === null) {
-          if (this.selectedResource) {
-            this.selectedResource.unselect();
-          }
-          return;
-        }
-
-        let entryKey = value.drawID;
-        if (this.data.values.selectedLayer < 9) {
-          entryKey += 100;
-        }
-
-        let resource = this.selectedLayer.entries.get(entryKey);
+        this.selectedLayer.selectEntry(value.drawID);
+        let resource = this.selectedLayer.selectedEntry;
         if (resource) {
-          resource.select();
           this.selectedLayer.scroll = resource.y;
           this.updateScroll();
         }
+        this.updateSelectedDrawID();
       }
     );
 
@@ -422,26 +83,44 @@ export class PaletteScene extends Phaser.Scene {
       }
     );
 
+    this.input.on("pointerup", (pointer) => {
+      if (pointer.leftButtonReleased() && pointer.getDistance() < 16) {
+        let entry = this.selectedLayer.getEntryAtPosition(
+          pointer.x,
+          pointer.y + this.cameras.main.scrollY
+        );
+        if (entry) {
+          this.selectedLayer.selectEntry(entry.id);
+          this.updateSelectedDrawID();
+        }
+      }
+    });
+
     this.scale.on("resize", this.resize, this);
+
+    this.selectLayer(this.data.values.selectedLayer);
     this.resize();
   }
 
   createLayers() {
     let result = [];
+
     for (let fileID of [3, 4, 5, 6, 6, 7, 3, 22, 5]) {
       result.push(this.createResourceLayer(fileID));
     }
+
     result.push(this.createSpecLayer());
+
     return result;
   }
 
   createResourceLayer(fileID) {
-    let layer = new PaletteLayer(this);
+    let layer = this.add.paletteLayer(this);
     let gfxLoader = this.data.values.gfxLoader;
     let resourceIDs = gfxLoader.resourceIDs(fileID);
 
     if (fileID === 3) {
-      layer.entries.set(100, new PaletteLayerBlackTileEntry(layer));
+      layer.addEntry(new PaletteLayerBlackTileEntry(this.textureCache));
     }
 
     for (let resourceID of resourceIDs) {
@@ -463,59 +142,50 @@ export class PaletteScene extends Phaser.Scene {
       }
 
       let resource = new PaletteLayerResourceEntry(
-        layer,
+        this.textureCache,
         width,
         height,
         fileID,
         resourceID
       );
 
-      layer.entries.set(resourceID, resource);
+      layer.addEntry(resource);
     }
 
     return layer;
   }
 
   createSpecLayer() {
-    let layer = new PaletteLayer(this);
+    let layer = this.add.paletteLayer(this);
 
     for (let tileSpec = 0; tileSpec < 37; ++tileSpec) {
-      let spec = new PaletteLayerSpecEntry(layer, tileSpec);
-
-      layer.entries.set(tileSpec, spec);
+      let spec = new PaletteLayerSpecEntry(this.textureCache, tileSpec);
+      layer.addEntry(spec);
     }
 
     return layer;
   }
 
-  // FIXME: This is a stupid idea.
-  createMasterAnimation() {
-    let cacheEntry = this.textureCache.getCursor();
-    cacheEntry.incRef();
+  selectLayer(layer) {
+    if (this.selectedLayer) {
+      this.selectedLayer.visible = false;
+    }
 
-    let frames = cacheEntry.asset.frames.map((f) => ({
-      key: f.texture.key,
-      frame: f.name,
-    }));
-    frames.pop();
+    this.selectedLayer = this.layers[layer];
+    this.selectedLayer.visible = true;
 
-    let animation = this.game.anims.create({
-      key: "masterAnimation",
-      frames: frames,
-      frameRate: 1.66,
-      repeat: -1,
-    });
-
-    return this.add.sprite(0, -100).setVisible(false).play(animation).anims;
+    this.prioritizePreloads();
+    this.updateScroll();
+    this.updateSelectedDrawID();
+    this.updateContentHeight();
   }
 
   update(_time, _delta) {
-    if (this.cameras.main.dirty) {
-      this.selectedLayer.cull();
-    }
+    this.selectedLayer.update(_time, _delta);
 
     if (this.selectedLayer.dirty && this.canDoResizeLayout()) {
       this.selectedLayer.layout();
+      this.updateContentHeight();
     }
 
     if (this.preloads.length > 0) {
@@ -558,10 +228,13 @@ export class PaletteScene extends Phaser.Scene {
     this.lastResize = performance.now();
   }
 
-  syncToMasterAnimation(sprite) {
-    if (sprite.anims.isPlaying) {
-      sprite.anims.setProgress(this.masterAnimation.getProgress());
-      sprite.anims.accumulator = this.masterAnimation.accumulator;
+  prioritizePreloads() {
+    let preloads = this.preloads;
+    for (let preload of preloads) {
+      if (preload.layer === this.selectedLayer) {
+        Phaser.Utils.Array.MoveTo(preloads, preload, 0);
+        break;
+      }
     }
   }
 
@@ -575,17 +248,13 @@ export class PaletteScene extends Phaser.Scene {
 
   updateSelectedDrawID() {
     let id = null;
-    if (this.selectedResource) {
-      id = this.selectedResource.id;
+    if (this.selectedLayer.selectedEntry) {
+      id = this.selectedLayer.selectedEntry.id;
     }
     this.data.set("selectedDrawID", id);
   }
 
-  get selectedLayer() {
-    return this.layers[this.data.values.selectedLayer];
-  }
-
-  get selectedResource() {
-    return this.selectedLayer.selectedResource;
+  updateContentHeight() {
+    this.data.set("contentHeight", this.selectedLayer.height);
   }
 }
