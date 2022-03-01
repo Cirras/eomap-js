6ef3d498ca6f559cc3886e1a1220c6b2b48829ea
Jonah Jeleniewski <cirrasbu@hotmail.com>
2022-03-02 02:39:11 +1100

    Better file hashes
    The EO client's reader interprets a zero-byte to mean EOF.
    A standard crc32 can produce zero-bytes, causing the client to truncate the EMF in the first few bytes and display a blank map.
    The 4 CRC32 bytes are now split and written out as two EO-encoded shorts.

--- src/data/eo-builder.js
+++ src/data/eo-builder.js
@@ -1,6 +1,7 @@
 import { encodeNumber } from "./eo-encode";
 
 import crc32 from "crc-32";
+import { SHORT_MAX } from "./eo-numeric-limits";
 
 export class EOBuilder {
   constructor() {
@@ -51,16 +52,25 @@ export class EOBuilder {
 
   addHash() {
     this.hashPos = this.data.length;
-    this.addInt(0);
+    this.addShort(0);
+    this.addShort(0);
   }
 
   build() {
     let data = Uint8Array.from(this.data);
+
     if (this.hashPos !== null) {
       let hash = crc32.buf(data);
-      let bytes = encodeNumber(hash);
-      data.set(bytes, this.hashPos);
+      let bytes = new Uint8Array(4);
+      let dataView = new DataView(bytes.buffer);
+      dataView.setUint32(0, hash, true);
+
+      let rid1 = encodeNumber(dataView.getUint16(0, true));
+      let rid2 = encodeNumber(dataView.getUint16(2, true));
+
+      data.set([rid1[0], rid1[1], rid2[0], rid2[1]], this.hashPos);
     }
+
     return data;
   }
 }
