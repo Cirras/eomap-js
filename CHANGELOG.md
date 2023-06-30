66dff336eb2a5365fd0a9f8ea63ceb0468e71ceb
Jonah Jeleniewski <cirrasbu@hotmail.com>
2023-06-30 20:06:06 +1000

    Fix bug where fill tool would use the wrong map graphic

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Fix bug where fill tool would use the wrong map graphic.
+
 ## [1.1.0] - 2023-06-29
 
 ### Added
--- src/core/tools/fill-tool.js
+++ src/core/tools/fill-tool.js
@@ -2,10 +2,6 @@ import { Tool } from "./tool";
 
 export class FillTool extends Tool {
   handleLeftPointerDown(mapEditor) {
-    mapEditor.doFillCommand(
-      mapEditor.currentPos.x,
-      mapEditor.currentPos.y,
-      mapEditor.selectedDrawID
-    );
+    mapEditor.doFillCommand(mapEditor.selectedDrawID);
   }
 }
